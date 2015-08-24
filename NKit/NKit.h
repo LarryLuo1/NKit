1efa44bbad2efb1a62cc335f1335b2662f49fde9
jame298 <jame2981@gmail.com>
2015-08-24 10:52:32 +0800

    init

---
+++ NKit/NKit.h
@@ -0,0 +1,13 @@
+//
+//  NKit.h
+//  NKit
+//
+//  Created by 建 张 on 15/8/24.
+//  Copyright (c) 2015年 Jame298. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NKit : NSObject
+
+@end
---
+++ NKit/NKit.m
@@ -0,0 +1,13 @@
+//
+//  NKit.m
+//  NKit
+//
+//  Created by 建 张 on 15/8/24.
+//  Copyright (c) 2015年 Jame298. All rights reserved.
+//
+
+#import "NKit.h"
+
+@implementation NKit
+
+@end
